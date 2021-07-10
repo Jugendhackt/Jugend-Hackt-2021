3d96e2c2e71badd951e13846ddaee53f09ae4c8b
BuildTools <unconfigured@null.spigotmc.org>
2021-07-10 13:58:22 +0200

    Let's go

--- server.js
+++ server.js
@@ -2,20 +2,16 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app)
-const { Server } = require('socket.io')
-const io = new Server(server);
 
 app.use('/frontend', express.static(`${__dirname}` + '/frontend'));
 
+app.use(require('express-basic-auth')({
+    users: { 'BrandV': 'BrandV' },
+    challenge: true
+}));
+
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}` + '\\frontend\\index.html');
-});
-
-io.on('connection', (socket) => {
-    console.log('Ein Nutzer hat den Chat betreten!');
-    socket.on('disconnect', () => {
-        console.log('Ein User hat den Raum verlassen!');
-    });
 });
 
 server.listen(8080, () => {
