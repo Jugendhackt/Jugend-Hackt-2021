a902f850430518d46eef8cf0087cf94c10b0a9ec
BuildTools <unconfigured@null.spigotmc.org>
2021-07-10 16:37:02 +0200

    Der Web-Chat geht jetzt

--- Python Bot/training.py
+++
@@ -1,7 +0,0 @@
-import random
-import json
-import pickle
-import numpy as np
-
-import nltk
-from nltk.stem import WordNetLemmatizer
--- server.js
+++ server.js
@@ -2,16 +2,24 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app)
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.use('/frontend', express.static(`${__dirname}` + '/frontend'));
-
-app.use(require('express-basic-auth')({
-    users: { 'BrandV': 'BrandV' },
-    challenge: true
-}));
+app.use('/views', express.static(`${__dirname}` + '/views'));
 
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}` + '\\frontend\\index.html');
+    res.sendFile(`${__dirname}` + '\\views\\chat.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('Ein Nutzer ist Connected');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+io.on('disconnect', (socket) => {
+    console.log('Ein Nutzer ist Disconnected');
 });
 
 server.listen(8080, () => {
